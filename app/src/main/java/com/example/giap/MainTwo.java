eb52dbb2cd20e628f770d2014c9f849ff69ac09c
Daniele Pandolfo <cocomelone46@gmail,com>
2021-12-14 16:19:07 +0100

    imageprova

--- app/src/main/java/com/example/giap/MainTwo.java
+++ app/src/main/java/com/example/giap/MainTwo.java
@@ -4,24 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainTwo extends AppCompatActivity {
 TextView tt;
+ImageView ii;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_two);
         tt=(TextView)findViewById(R.id.textVie);
+        ii=(ImageView)findViewById(R.id.imageView);
 
 
 
         //passo degli attributi alla seconda activiti, come???
-        Intent coc = getIntent();
-        String messaggioricevuto = coc.getStringExtra("messaggio");
-        tt.setText("ciaoooooooooooool");
-        Toast.makeText(getApplicationContext(), "titolo", Toast.LENGTH_LONG).show();
+        Intent cioc = getIntent();
+        String messaggioricevuto = cioc.getStringExtra("messaggio");
+        tt.setText(messaggioricevuto);
+        Toast.makeText(getApplicationContext(), messaggioricevuto, Toast.LENGTH_LONG).show();
         //tostring---->
     }
 }
